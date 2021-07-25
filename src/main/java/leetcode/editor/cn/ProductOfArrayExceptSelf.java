641f6ed69443945206453e4ce9758e50ae882b06
PerfySchu <811601609@qq.com>
2021-07-25 23:20:05 +0800

    update daily solution

---
+++ src/main/java/leetcode/editor/cn/ProductOfArrayExceptSelf.java
@@ -0,0 +1,56 @@
+//第238题
+//给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之
+//外其余各元素的乘积。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: [1,2,3,4]
+//输出: [24,12,8,6] 
+//
+// 
+//
+// 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。 
+//
+// 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。 
+//
+// 进阶： 
+//你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。） 
+// Related Topics 数组 前缀和 
+// 👍 865 👎 0
+
+package leetcode.editor.cn;
+public class ProductOfArrayExceptSelf{
+    public static void main(String[] args){
+        Solution solution = new ProductOfArrayExceptSelf().new Solution();
+        System.out.println(solution.productExceptSelf(new int[]{1,2,3,4,5}));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
