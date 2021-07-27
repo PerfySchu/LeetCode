2bff9ba2b7d883245141b8ae389a584b8c41bc3a
PerfySchu <811601609@qq.com>
2021-07-27 20:10:31 +0800

    daily solution

---
+++ src/main/java/leetcode/editor/cn/SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,100 @@
+//第671题
+//给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一
+//个。 
+//
+// 更正式地说，root.val = min(root.left.val, root.right.val) 总成立。 
+//
+// 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [2,2,5,null,null,5,7]
+//输出：5
+//解释：最小的值是 2 ，第二小的值是 5 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,2,2]
+//输出：-1
+//解释：最小的值是 2, 但是不存在第二小的值。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 25] 内 
+// 1 <= Node.val <= 231 - 1 
+// 对于树中每个节点 root.val == min(root.left.val, root.right.val) 
+// 
+// Related Topics 树 深度优先搜索 二叉树 
+// 👍 187 👎 0
+
+package leetcode.editor.cn;
+public class SecondMinimumNodeInABinaryTree{
+    public static void main(String[] args){
+        Solution solution = new SecondMinimumNodeInABinaryTree().new Solution();
+        System.out.println(solution.findSecondMinimumValue(null));
+    }
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int res;
+    private int rootValue;
+    public int findSecondMinimumValue(TreeNode root) {
+        res = -1;
+        rootValue = root.val;
+        dfs(root);
+        return res==root.val ? -1 : res;
+    }
+
+    public void dfs(TreeNode node){
+        if(node == null){
+            return;
+        }
+        if(res != -1 && node.val>= res){
+            return;
+        }
+        if(node.val > rootValue){
+            res = node.val;
+        }
+        dfs(node.left);
+        dfs(node.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
