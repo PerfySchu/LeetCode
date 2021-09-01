929a3db02ff64b005c457a36a538bd0faed6b430
PerfySchu <811601609@qq.com>
2021-09-01 09:11:35 +0800

    daily solution 20210901

--- src/main/java/leetcode/editor/cn/CompareVersionNumbers.java
+++ src/main/java/leetcode/editor/cn/CompareVersionNumbers.java
@@ -109,4 +109,31 @@ class Solution {
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+//优雅一点写法
+public static int solution2(String version1, String version2){
+    String[] v1 = version1.split("\\.");
+    String[] v2 = version2.split("\\.");
+    int i = 0;
+    while (i<v1.length || i<v2.length){
+        //设置默认值，优雅 ！！！
+        int v1Num = 0;
+        int v2Num = 0;
+        if(i < v1.length){
+            v1Num = Integer.parseInt(v2[i]);
+        }
+        if(i < v2.length){
+            v2Num = Integer.parseInt(v1[i]);
+        }
+        if(v1Num>v2Num){
+            return 1;
+        }
+        if(v1Num < v2Num){
+            return -1;
+        }
+        i++;
+    }
+    return 0;
 }
+
+}
+
