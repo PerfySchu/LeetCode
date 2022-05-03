30de195f2c053f5ddb2b42fe64f712fd48f516a9
舒鹏飞 <shupengfei@yingmi.cn>
2022-05-03 21:23:40 +0800

    daily solution 20220503

---
+++ src/main/java/leetcode/editor/cn/ReorderDataInLogFiles.java
@@ -0,0 +1,97 @@
+//2022-05-03 21:08:15
+//第937题
+//给你一个日志数组 logs。每条日志都是以空格分隔的字串，其第一个字为字母与数字混合的 标识符 。
+//
+// 有两种不同类型的日志：
+//
+//
+// 字母日志：除标识符之外，所有字均由小写字母组成
+// 数字日志：除标识符之外，所有字均由数字组成
+//
+//
+// 请按下述规则将日志重新排序：
+//
+//
+// 所有 字母日志 都排在 数字日志 之前。
+// 字母日志 在内容不同时，忽略标识符后，按内容字母顺序排序；在内容相同时，按标识符排序。
+// 数字日志 应该保留原来的相对顺序。
+//
+//
+// 返回日志的最终顺序。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3
+//art zero"]
+//输出：["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6
+//"]
+//解释：
+//字母日志的内容都不同，所以顺序为 "art can", "art zero", "own kit dig" 。
+//数字日志保留原来的相对顺序 "dig1 8 1 5 1", "dig2 3 6" 。
+//
+//
+// 示例 2：
+//
+//
+//输入：logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+//
+//输出：["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= logs.length <= 100
+// 3 <= logs[i].length <= 100
+// logs[i] 中，字与字之间都用 单个 空格分隔
+// 题目数据保证 logs[i] 都有一个标识符，并且在标识符之后至少存在一个字
+//
+// Related Topics 数组 字符串 排序 👍 172 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReorderDataInLogFiles{
+    public static void main(String[] args){
+        Solution solution = new ReorderDataInLogFiles().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    class Log {
+        int type, idx;
+        String ori, sign, content;
+        Log(String s, int _idx) {
+            idx = _idx;
+            int n = s.length(), i = 0;
+            while (i < n && s.charAt(i) != ' ') i++;
+            sign = s.substring(0, i);
+            content = s.substring(i + 1);
+            ori = s;
+            type = Character.isDigit(content.charAt(0)) ? 1 : 0;
+        }
+    }
+    public String[] reorderLogFiles(String[] logs) {
+        int n = logs.length;
+        List<Log> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) list.add(new Log(logs[i], i));
+        Collections.sort(list, (a, b)->{
+            if (a.type != b.type) return a.type - b.type;
+            if (a.type == 1) return a.idx - b.idx;
+            return !a.content.equals(b.content) ? a.content.compareTo(b.content) : a.sign.compareTo(b.sign);
+        });
+        String[] ans = new String[n];
+        for (int i = 0; i < n; i++) ans[i] = list.get(i).ori;
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
