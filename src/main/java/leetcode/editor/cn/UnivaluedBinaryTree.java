d6e3dd69f976d19a0e5109d2d1f18f5157dc88ab
舒鹏飞 <shupengfei@yingmi.cn>
2022-05-24 09:28:27 +0800

    daily solution 20220524

---
+++ src/main/java/leetcode/editor/cn/UnivaluedBinaryTree.java
@@ -0,0 +1,83 @@
+//2022-05-24 09:15:20
+//第965题
+//如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+//
+// 只有给定的树是单值二叉树时，才返回 true；否则返回 false。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+// 输入：[1,1,1,1,1,null,1]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//
+// 输入：[2,2,2,5,2]
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 给定树的节点数范围是 [1, 100]。
+// 每个节点的值都是整数，范围为 [0, 99] 。
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 122 👎 0
+
+package leetcode.editor.cn;
+public class UnivaluedBinaryTree{
+    public static void main(String[] args){
+        Solution solution = new UnivaluedBinaryTree().new Solution();
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        boolean ans = true;
+        if (root.left != null){
+            ans = root.val == root.left.val && isUnivalTree(root.left);
+        }
+        if(root.right != null){
+            ans = ans && root.val == root.right.val && isUnivalTree(root.right);
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
