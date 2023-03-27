5e5ccc6fac40ac851c94811ce50b7057d6ec71da
舒鹏飞 <shupengfei@yingmi.cn>
2023-03-27 14:48:26 +0800

    daily solution20230327

--- src/main/java/leetcode/editor/cn/CountSubstringsThatDifferByOneCharacter.java
+++ src/main/java/leetcode/editor/cn/CountSubstringsThatDifferByOneCharacter.java
@@ -67,12 +67,27 @@ package leetcode.editor.cn;
 public class CountSubstringsThatDifferByOneCharacter{
     public static void main(String[] args){
         Solution solution = new CountSubstringsThatDifferByOneCharacter().new Solution();
+        System.out.println(solution.countSubstrings("aba", "baba"));
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int countSubstrings(String s, String t) {
-
-        return 0;
+        int m = s.length(), n = t.length();
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int diff = 0;
+                for (int k = 0; i + k < m && j + k < n; k++) {
+                    diff += s.charAt(i + k) == t.charAt(j + k) ? 0 : 1;
+                    if (diff > 1) {
+                        break;
+                    } else if (diff == 1) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
