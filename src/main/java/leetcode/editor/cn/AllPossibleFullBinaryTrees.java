f2395fd338e076584a83c5b179115f9f9f32067a
舒鹏飞 <shupengfei@yingmi.cn>
2024-04-02 15:17:12 +0800

    daily solution 20240402

---
+++ src/main/java/leetcode/editor/cn/AllPossibleFullBinaryTrees.java
@@ -0,0 +1,98 @@
+//2024-04-02 09:43:25
+//第894题
+//给你一个整数 n ，请你找出所有可能含 n 个节点的 真二叉树 ，并以列表形式返回。答案中每棵树的每个节点都必须符合 Node.val == 0 。
+//
+// 答案的每个元素都是一棵真二叉树的根节点。你可以按 任意顺序 返回最终的真二叉树列表。
+//
+// 真二叉树 是一类二叉树，树中每个节点恰好有 0 或 2 个子节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 7
+//输出：[[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0
+//,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 3
+//输出：[[0,0,0]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 20
+//
+//
+// Related Topics 树 递归 记忆化搜索 动态规划 二叉树 👍 340 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPossibleFullBinaryTrees{
+    public static void main(String[] args){
+        Solution solution = new AllPossibleFullBinaryTrees().new Solution();
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> fbt = new ArrayList<>();
+        if (n%2 == 0) {
+            return fbt;
+        }
+        if (n == 1) {
+            fbt.add(new TreeNode(0));
+            return fbt;
+        }
+        for (int i = 1; i < n; i++) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(n - 1 - i);
+            for (TreeNode leftNode : left) {
+                for (TreeNode rightNode : right) {
+                    TreeNode root = new TreeNode(0, leftNode, rightNode);
+                    fbt.add(root);
+                }
+            }
+        }
+        return fbt;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
