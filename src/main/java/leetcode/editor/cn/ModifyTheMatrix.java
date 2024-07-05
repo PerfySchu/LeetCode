3f1eb0c5cb6739bdd40f12e4d54670264829dece
舒鹏飞 <shupengfei@yingmi.cn>
2024-07-05 10:21:48 +0800

    daily solution20240705

--- src/main/java/leetcode/editor/cn/ModifyTheMatrix.java
+++ src/main/java/leetcode/editor/cn/ModifyTheMatrix.java
@@ -54,16 +54,14 @@ class Solution {
     public int[][] modifiedMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] max = new int[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                max[j] = Math.max(max[j], matrix[i][j]);
+        for (int j = 0; j < n; j++) {
+            int colMap = -1;
+            for (int[] row : matrix) {
+                colMap = Math.max(colMap, row[j]);
             }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
                 if (matrix[i][j] == -1) {
-                    matrix[i][j] = max[j];
+                    matrix[i][j] = colMap;
                 }
             }
         }
