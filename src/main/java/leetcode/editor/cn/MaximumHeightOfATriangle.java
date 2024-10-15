1417a0b77b345230f30ffd72a538bfddbeecf901
舒鹏飞 <shupengfei@yingmi.cn>
2024-10-15 14:04:16 +0800

    daily solution20241015

---
+++ src/main/java/leetcode/editor/cn/MaximumHeightOfATriangle.java
@@ -0,0 +1,126 @@
+//2024-10-15 10:05:59
+//第3200题
+//给你两个整数 red 和 blue，分别表示红色球和蓝色球的数量。你需要使用这些球来组成一个三角形，满足第 1 行有 1 个球，第 2 行有 2 个球，第
+//3 行有 3 个球，依此类推。
+//
+// 每一行的球必须是 相同 颜色，且相邻行的颜色必须 不同。
+//
+// 返回可以实现的三角形的 最大 高度。
+//
+//
+//
+// 示例 1：
+//
+//
+// 输入： red = 2, blue = 4
+//
+//
+// 输出： 3
+//
+// 解释：
+//
+//
+//
+// 上图显示了唯一可能的排列方式。
+//
+// 示例 2：
+//
+//
+// 输入： red = 2, blue = 1
+//
+//
+// 输出： 2
+//
+// 解释：
+//
+// 上图显示了唯一可能的排列方式。
+//
+// 示例 3：
+//
+//
+// 输入： red = 1, blue = 1
+//
+//
+// 输出： 1
+//
+// 示例 4：
+//
+//
+// 输入： red = 10, blue = 1
+//
+//
+// 输出： 2
+//
+// 解释：
+//
+// 上图显示了唯一可能的排列方式。
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= red, blue <= 100
+//
+//
+// Related Topics 数组 枚举 👍 14 👎 0
+
+package leetcode.editor.cn;
+public class MaximumHeightOfATriangle{
+    public static void main(String[] args){
+        Solution solution = new MaximumHeightOfATriangle().new Solution();
+        System.out.println(solution.maxHeightOfTriangle(1,1));
+        System.out.println(solution.maxHeightOfTriangle(2,4));
+        System.out.println(solution.maxHeightOfTriangle(10, 1));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxHeightOfTriangle(int red, int blue) {
+        int red2 = red;
+        int blue2 = blue;
+
+        //红色在上
+        int ans1 = 0;
+        int level1 = 1;
+        while(red>0){
+            if (red>=level1) {
+                ans1++;
+                red-=level1;
+                level1++;
+            }else{
+                break;
+            }
+            if(blue>=level1){
+                ans1++;
+                blue-=level1;
+                level1++;
+            }else{
+                break;
+            }
+        }
+
+        // 蓝色在上
+        int ans2 = 0;
+        int level2 = 1;
+        while(blue2>0){
+            if (blue2>=level2) {
+                ans2++;
+                blue2-=level2;
+                level2++;
+            }else{
+                break;
+            }
+            if(red2>=level2){
+                ans2++;
+                red2-=level2;
+                level2++;
+            }else{
+                break;
+            }
+        }
+        return Math.max(ans1, ans2);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
