c3b5073bfd5e615c4cc8024e13116dc9eea88174
舒鹏飞 <shupengfei@yingmi.cn>
2025-02-08 14:37:56 +0800

    daily solution20250108

---
+++ src/main/java/leetcode/editor/cn/UniquePathsIi.java
@@ -0,0 +1,80 @@
+//2025-02-08 14:35:21
+//第63题
+//给定一个 m x n 的整数数组 grid。一个机器人初始位于 左上角（即 grid[0][0]）。机器人尝试移动到 右下角（即 grid[m - 1][
+//n - 1]）。机器人每次只能向下或者向右移动一步。
+//
+// 网格中的障碍物和空位置分别用 1 和 0 来表示。机器人的移动路径中不能包含 任何 有障碍物的方格。
+//
+// 返回机器人能够到达右下角的不同路径数量。
+//
+// 测试用例保证答案小于等于 2 * 10⁹。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+//输出：2
+//解释：3x3 网格的正中间有一个障碍物。
+//从左上角到右下角一共有 2 条不同的路径：
+//1. 向右 -> 向右 -> 向下 -> 向下
+//2. 向下 -> 向下 -> 向右 -> 向右
+//
+//
+// 示例 2：
+//
+//
+//输入：obstacleGrid = [[0,1],[0,0]]
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] 为 0 或 1
+//
+//
+// Related Topics 数组 动态规划 矩阵 👍 1357 👎 0
+
+package leetcode.editor.cn;
+public class UniquePathsIi{
+    public static void main(String[] args){
+        Solution solution = new UniquePathsIi().new Solution();
+        System.out.println(solution.uniquePathsWithObstacles(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+        System.out.println(solution.uniquePathsWithObstacles(new int[][]{{0,1},{0,0}}));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
