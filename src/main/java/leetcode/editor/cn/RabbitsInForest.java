a1b0270f002a2c3f9910ab622a5960ca6c7349af
舒鹏飞 <shupengfei@yingmi.cn>
2025-04-20 15:15:59 +0800

    daily solution20250420

---
+++ src/main/java/leetcode/editor/cn/RabbitsInForest.java
@@ -0,0 +1,76 @@
+//2025-04-20 15:05:12
+//第781题
+//森林中有未知数量的兔子。提问其中若干只兔子 "还有多少只兔子与你（指被提问的兔子）颜色相同?" ，将答案收集到一个整数数组 answers 中，其中
+//answers[i] 是第 i 只兔子的回答。
+//
+// 给你数组 answers ，返回森林中兔子的最少数量。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：answers = [1,1,2]
+//输出：5
+//解释：
+//两只回答了 "1" 的兔子可能有相同的颜色，设为红色。
+//之后回答了 "2" 的兔子不会是红色，否则他们的回答会相互矛盾。
+//设回答了 "2" 的兔子为蓝色。
+//此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。
+//因此森林中兔子的最少数量是 5 只：3 只回答的和 2 只没有回答的。
+//
+//
+// 示例 2：
+//
+//
+//输入：answers = [10,10,10]
+//输出：11
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= answers.length <= 1000
+// 0 <= answers[i] < 1000
+//
+//
+// Related Topics 贪心 数组 哈希表 数学 👍 301 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RabbitsInForest{
+    public static void main(String[] args){
+        Solution solution = new RabbitsInForest().new Solution();
+        System.out.println(solution.numRabbits(new int[]{1, 1, 2}));
+        System.out.println(solution.numRabbits(new int[]{10, 10, 10}));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int answer : answers) {
+            map.put(answer, map.getOrDefault(answer, 0) + 1);
+        }
+        int res = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int k = entry.getKey();
+            int v = entry.getValue();
+            if(v <= k+1){
+                res += k+1;
+            }else if(v % (k+1) == 0){
+                res += v;
+            }else {
+                res += (v / (k+1) + 1) * (k+1);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
