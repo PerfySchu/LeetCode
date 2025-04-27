60badae28b67eb4baef04ef2143a6d496a2875c8
舒鹏飞 <shupengfei@yingmi.cn>
2025-04-27 09:25:18 +0800

    daily solution20250427

--- src/main/java/leetcode/editor/cn/CountSubarraysOfLengthThreeWithACondition.java
+++ src/main/java/leetcode/editor/cn/CountSubarraysOfLengthThreeWithACondition.java
@@ -54,8 +54,7 @@ class Solution {
     public int countSubarrays(int[] nums) {
         int ans = 0;
         for (int i = 1; i < nums.length - 1; i++) {
-            int sum = nums[i - 1] + nums[i + 1];
-            if (sum == nums[i] / 2 && nums[i] % 2 == 0) {
+            if ((nums[i - 1] + nums[i + 1]) * 2 == nums[i]) {
                 ans++;
             }
         }
